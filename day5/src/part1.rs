4322cabdd1ed0c75aea73468d7575d4660d0418e
Luke Harding <lukeharding990@gmail.com>
2023-12-09 00:03:26 -0500

    Day 5 Part 1 works; Part 2 ?

--- day5/src/part1.rs
+++ day5/src/part1.rs
@@ -1,16 +1,15 @@
 use std::fs;
 use std::io;
 use std::io::{Error, ErrorKind};
-use std::ops::Range;
 use std::collections::HashMap;
 
 pub fn part1() -> io::Result<()> {
     let almanac = read_almanac()?;
 
-    let mut table: HashMap<u32, Vec<u32>> = HashMap::new();
+    let mut locations: HashMap<u64, u64> = HashMap::new();
 
     for seed in almanac.seeds.iter() {
-        let mut seed_vec: Vec<u32> = vec![*seed];
+        let mut seed_vec: Vec<u64> = vec![*seed];
 
         for (i, map) in almanac.maps.iter().enumerate() {
             let operator = match seed_vec.last().cloned() {
@@ -20,49 +19,65 @@ pub fn part1() -> io::Result<()> {
 
             for row in map.rows.iter() {
                 if (row.src_range_start..row.src_range_start + row.range_length).contains(&operator) {
-                    if *seed >= row.src_range_start {
-                        seed_vec.push((seed - row.src_range_start) + row.dest_range_start);
-                    }
+                    let offset = match (operator - row.src_range_start).checked_add(row.dest_range_start) {
+                        Some(x) => x,
+                        None => return Err(Error::new(ErrorKind::Other, format!("OVERFLOW {} + {1}", operator - row.src_range_start, row.dest_range_start)))
+                    };
+
+                    seed_vec.push(offset);
                 }
             }
 
-            if seed_vec.len() != i + 1 {
+            if seed_vec.len() != i + 2 {
                 seed_vec.push(operator);
             }
         }
 
-        table.insert(*seed, seed_vec);
+        let location = match seed_vec.last() {
+            Some(x) => x,
+            None => return Err(Error::new(ErrorKind::Other, "No Last Element"))
+        };
+
+        locations.insert(*seed, *location);
     }
 
-    println!("{:?}", table);
+    let mut smallest: Option<u64> = None;
+
+    for (_, location) in locations.iter() {
+        if smallest.is_none() {
+            smallest = Some(*location);
+        } else if smallest.is_some_and(|x| x > *location) {
+            smallest = Some(*location);
+        }
+    }
+
+    println!("{}", smallest.unwrap());
 
     Ok(())
 }
 
 #[derive(Debug)]
 struct Almanac {
-    seeds: Vec<u32>,
+    seeds: Vec<u64>,
     maps: Vec<Map>
 }
 
 #[derive(Debug)]
 struct Map {
-    source_type: String,
-    destination_type: String,
     rows: Vec<Row>
 }
 
 #[derive(Debug)]
 struct Row {
-    dest_range_start: u32,
-    src_range_start: u32,
-    range_length: u32
+    dest_range_start: u64,
+    src_range_start: u64,
+    range_length: u64
 }
 
 fn read_almanac() -> io::Result<Almanac> {
-    let text: String = fs::read_to_string("./inputTest.txt")?;
+    let text: String = fs::read_to_string("./input.txt")?;
 
-    let mut seeds: Vec<u32> = vec![]; 
+    let mut seeds: Vec<u64> = vec![]; 
     let mut maps: Vec<Map> = vec![];
 
     let lines: Vec<String> = text.lines().map(String::from).collect();
@@ -70,16 +85,13 @@ fn read_almanac() -> io::Result<Almanac> {
     for (row, line) in lines.iter().enumerate() {
         if line.starts_with("seeds: ") {
             let (_, seed_string) = line.split_at(7);
-            seeds = seed_string.to_string().split_whitespace().flat_map(|s| s.parse::<u32>()).collect();
+            seeds = seed_string.to_string().split_whitespace().flat_map(|s| s.parse::<u64>()).collect();
         } else if line.starts_with(|c: char| c.is_alphabetic()) {
-            let (map_string, _) = line.split_at(line.len() - 5);
-            let map_types: Vec<String> = map_string.split("-to-").map(String::from).collect();
-
             let mut i = row + 1;
             let mut rows: Vec<Row> = vec![];
             while i < lines.len() && !lines[i].eq("") {
                 let row_line = lines[i].clone();
-                let numbers: Vec<u32> = row_line.split_whitespace().flat_map(|s| s.parse::<u32>()).collect();
+                let numbers: Vec<u64> = row_line.split_whitespace().flat_map(|s| s.parse::<u64>()).collect();
 
                 rows.push(Row {
                     dest_range_start: numbers[0],
@@ -91,8 +103,6 @@ fn read_almanac() -> io::Result<Almanac> {
             }
 
             maps.push(Map {
-                source_type: map_types[0].clone(),
-                destination_type: map_types[1].clone(),
                 rows
             });
         }
--- day5/src/part2.rs
+++ day5/src/part2.rs
@@ -1,3 +1,131 @@
-pub fn part2() {
+use std::fs;
+use std::io;
+use std::io::{Error, ErrorKind};
+use std::ops::Range;
+use rayon::prelude::*;
 
+pub fn part2() -> io::Result<()> {
+    let almanac = read_almanac()?;
+
+    let mut locations: Vec<u64> = vec![];
+
+    for seed_range in almanac.seeds.iter() {
+        println!("Range {} .. {1}", seed_range.start, seed_range.end);
+        let range = seed_range.clone();
+        let location_list: Vec<u64> = range.into_par_iter().flat_map(|seed| {
+            // println!("Seed {}; Range {1} .. {2}", seed, seed_range.start, seed_range.end);
+            let mut seed_vec: Vec<u64> = vec![seed];
+
+            for (i, map) in almanac.maps.iter().enumerate() {
+                let operator = match seed_vec.last().cloned() {
+                    Some(x) => x,
+                    None => return Err(Error::new(ErrorKind::Other, "No Last Element"))
+                };
+
+                for row in map.rows.iter() {
+                    if (row.src_range_start..row.src_range_start + row.range_length).contains(&operator) {
+                        let offset = match (operator - row.src_range_start).checked_add(row.dest_range_start) {
+                            Some(x) => x,
+                            None => return Err(Error::new(ErrorKind::Other, format!("OVERFLOW {} + {1}", operator - row.src_range_start, row.dest_range_start)))
+                        };
+
+                        seed_vec.push(offset);
+                    }
+                }
+
+                if seed_vec.len() != i + 2 {
+                    seed_vec.push(operator);
+                }
+            }
+
+            let location = match seed_vec.last() {
+                Some(x) => x,
+                None => return Err(Error::new(ErrorKind::Other, "No Last Element"))
+            };
+
+            Ok(*location)
+        }).collect();
+
+        locations.extend(location_list);
+    }
+
+    let mut smallest: Option<u64> = None;
+
+    for location in locations.iter() {
+        if smallest.is_none() {
+            smallest = Some(*location);
+        } else if smallest.is_some_and(|x| x > *location) {
+            smallest = Some(*location);
+        }
+    }
+
+    println!("{}", smallest.unwrap());
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Almanac {
+    seeds: Vec<Range<u64>>,
+    maps: Vec<Map>
+}
+
+#[derive(Debug)]
+struct Map {
+    rows: Vec<Row>
+}
+
+#[derive(Debug)]
+struct Row {
+    dest_range_start: u64,
+    src_range_start: u64,
+    range_length: u64
+}
+
+fn read_almanac() -> io::Result<Almanac> {
+    let text: String = fs::read_to_string("./input.txt")?;
+
+    let mut seeds: Vec<Range<u64>> = vec![]; 
+    let mut maps: Vec<Map> = vec![];
+
+    let lines: Vec<String> = text.lines().map(String::from).collect();
+
+    for (row, line) in lines.iter().enumerate() {
+        if line.starts_with("seeds: ") {
+            let (_, seed_string) = line.split_at(7);
+            let seed_list: Vec<u64> = seed_string.to_string().split_whitespace().flat_map(|s| s.parse::<u64>()).collect();
+            for (i, seed) in seed_list.iter().enumerate() {
+                if i % 2 == 0 {
+                    let next = seed_list[i + 1];
+                    let range_end = seed + next;
+
+                    seeds.push(*seed..range_end);
+                }
+            }
+        } else if line.starts_with(|c: char| c.is_alphabetic()) {
+            let mut i = row + 1;
+            let mut rows: Vec<Row> = vec![];
+            while i < lines.len() && !lines[i].eq("") {
+                let row_line = lines[i].clone();
+                let numbers: Vec<u64> = row_line.split_whitespace().flat_map(|s| s.parse::<u64>()).collect();
+
+                rows.push(Row {
+                    dest_range_start: numbers[0],
+                    src_range_start: numbers[1],
+                    range_length: numbers[2]
+                });
+
+                i = i +1;
+            }
+
+            maps.push(Map {
+                rows
+            });
+        }
+    }
+
+    Ok(Almanac {
+        seeds,
+        maps
+    })
 }
